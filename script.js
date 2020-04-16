b983bef7678c7ab01776da68021823a27bd4decb
Muiduzzaman Mahim <47756767+mmzm96@users.noreply.github.com>
2020-04-16 11:51:42 -0400

    script

---
+++ script.js
@@ -0,0 +1,20 @@
+///age calculator
+
+function age() {
+
+    var year = prompt('Enter your birth year');
+    var age = (2020 - year) * 365;
+    var ageinyear = (2020 - year);
+    var ageinmonth = (2020 - year) * 12;
+    var h1 = document.createElement('h3');
+    var result = document.createTextNode(`Your birth year is ${year}.Your age is ${ageinyear} years or ${ageinmonth} months or ${age} days.`);
+    h1.setAttribute('id', 'age');
+    h1.appendChild(result);
+    document.getElementById('result').appendChild(h1);
+
+}
+
+function reset() {
+    location.reload(true);
+
+}
